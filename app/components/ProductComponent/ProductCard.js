5ec0926abaefe3ac5b0168764329d58d0a3b2513
Lanai Conceição <lanai.conceicao@mercadolivre.com>
2022-08-08 09:32:57 -0300

    Exercício da aula 03 - Usando Hooks (Exercício 2)

--- app/components/ProductComponent/ProductCard.js
+++ app/components/ProductComponent/ProductCard.js
@@ -1,11 +1,12 @@
 const React = require('react');
 const PropTypes = require('prop-types');
 
-const { useState } = React;
+const { useState, useEffect } = React;
 const Image = require('nordic/image');
 
 const ProductCard = ({ nome, srcImage, altImage, price, permalink, i18n }) => {
   const [favorite, setFavorite] = useState('Produto não é favorito');
+  const [textBtnFavorite, setTextBtnFavorite] = useState('Remover dos favoritos');
 
   // Exercício 1
   // Adicione um botão em cada card do produto com a legenda “Adicionar aos
@@ -15,6 +16,17 @@ const ProductCard = ({ nome, srcImage, altImage, price, permalink, i18n }) => {
       ? setFavorite('Produto favoritado ⭐')
       : setFavorite('Produto não é favorito'));
 
+  // Exercício 2
+  // Assim que o produto for adicionado aos favoritos, o texto do botão deverá mudar
+  // para “Remover dos favoritos” e deverá permitir a remoção do referido produto dos
+  // favoritos. Ao fazer isso, o texto deve mudar novamente e o botão deve nos permitir
+  // adicionar o produto novamente. Verifique se a lista de favoritos foi modificada
+  // corretamente em cada ação do usuário por meio do console.
+  useEffect(() => (
+    textBtnFavorite === 'Remover dos favoritos'
+      ? setTextBtnFavorite('Adicionar aos favoritos')
+      : setTextBtnFavorite('Remover dos favoritos')
+  ), [favorite]);
   return (
     <article tabIndex="0">
       <a href={permalink} ariaLabel="Abrir produto em nova aba">
@@ -22,7 +34,7 @@ const ProductCard = ({ nome, srcImage, altImage, price, permalink, i18n }) => {
         <Image src={srcImage} alt={`Imagem de ${altImage}`} />
         <p tabIndex="-1">R$ {price}</p>
       </a>
-      <button onClick={makeFavorite}>{i18n.gettext('Adicionar aos favoritos')}</button>
+      <button onClick={makeFavorite}>{i18n.gettext(`${textBtnFavorite}`)}</button>
       <p>{favorite}</p>
     </article>
   );
